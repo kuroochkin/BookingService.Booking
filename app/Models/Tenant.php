a418e0f72822c1164800862931a672387b67e99e
kurochkinvr <pin11@kurochkin@gmail.com>
2024-05-13 00:37:26 +0300

    #feature/1 *Сущность арендатора

---
+++ app/Models/Tenant.php
@@ -0,0 +1,11 @@
+﻿<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tenant extends Model
+{
+    use HasFactory;
+}
