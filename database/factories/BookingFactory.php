6e7282875dae0e7875604a93058fb5a3961cf45a
kurochkinvr <pin11@kurochkin@gmail.com>
2024-05-13 00:43:46 +0300

    #feature/1 *Сущность бронирования

---
+++ database/factories/BookingFactory.php
@@ -0,0 +1,21 @@
+﻿<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Booking>
+ */
+class BookingFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'start_date' => fake()->date('Y-m-d'),
+            'end_date' => fake()->date('Y-m-d'),
+            'accommodation_id' => fake()->numberBetween(1, 5),
+            'tenant_id' => fake()->numberBetween(1, 5),
+        ];
+    }
+}
