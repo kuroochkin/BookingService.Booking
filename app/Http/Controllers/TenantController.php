04d843586b3b1424bc8ffa92692b9be5dbbdb615
kurochkinvr <pin11@kurochkin@gmail.com>
2024-05-14 13:48:57 +0300

    #feature/3 *Сервис и контроллер арендатора

---
+++ app/Http/Controllers/TenantController.php
@@ -0,0 +1,25 @@
+﻿<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\TenantService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class TenantController extends BaseController
+{
+    public function __construct(private readonly TenantService $tenantService)
+    {
+    }
+
+    public function getById(Request $request): JsonResponse
+    {
+        $result = $this->tenantService->getById($request);
+
+        if (!$result) {
+            return response()->json(['error' => 'Ошибка получения жильца.'])->setEncodingOptions(JSON_UNESCAPED_UNICODE);
+        }
+
+        return response()->json($result->toJson())->setEncodingOptions(JSON_UNESCAPED_UNICODE);
+    }
+}
